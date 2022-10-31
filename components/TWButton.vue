409d96f27e816e083783a92081ab20a2d91d007f
Will Forbes <wforbes87@gmail.com>
2022-10-31 01:25:01 -0700

    add tailwind styled button component

---
+++ components/TWButton.vue
@@ -0,0 +1,19 @@
+<template>
+	<input
+		type="button"
+		:value="value"
+		@click="$emit('click')"
+		:disabled="disabled" 
+		class="inline-block px-5 py-2 mx-auto text-white bg-blue-600 rounded-full hover:bg-blue-700 md:mx-0 disabled:opacity-30"
+	/>
+</template>
+<script setup>
+
+const props = defineProps({
+	value: String,
+	disabled: Boolean
+})
+
+const emit = defineEmits(['click'])
+
+</script>
